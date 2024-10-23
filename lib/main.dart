904d523baf8a2e3d87479d422a542be0fde310a5
Rubesha <108724596+Rubesha27@users.noreply.github.com>
2024-10-23 12:34:00 +0500

    provider added to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:coffee_shop/firebase%20services/firebase_options.dart';
 import 'package:coffee_shop/models/coffee_shop.dart';
+import 'package:coffee_shop/pages/shop_page.dart';
 import 'package:coffee_shop/pages/signup_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:coffee_shop/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,11 +18,15 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => CoffeeShop(),
-      builder: (context, child) => const MaterialApp(
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => CoffeeShop()),
+        ChangeNotifierProvider(create: (context) => CartProvider()),
+      ],
+      child: const MaterialApp(
         debugShowCheckedModeBanner: false,
         home: SignUpForm(),
       ),
